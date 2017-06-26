98fa66b9ce74c95382c3ee22728a3cfb7abcf2e7
kirillRomanenko <romanenko.kirill1995@gmail.com>
2017-06-27 02:08:17 +0300

    залил учебный проект

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+<?php
+//$dat = date("d.m.y");
+//echo "$dat";
+
+//$dat = date("h:i:s");
+//echo "$dat";
+
+for ($i=1; $i<=5; $i++){
+    echo ($i * $i * $i);
+}
+
+?>
+</body>
+</html>
